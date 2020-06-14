c6138f87ffa6bb201154dd45054c030ba82f1fbb
Bhushan Patil <patilbhushanp@gmail.com>
2020-06-15 03:27:19 +0530

    Spring Boot Sample Application with Monitoring through prometheus

--- src/main/java/com/ravaan/prometheus/configuration/ApplicationConfiguration.java
+++ src/main/java/com/ravaan/prometheus/configuration/ApplicationConfiguration.java
@@ -3,7 +3,15 @@
  */
 package com.ravaan.prometheus.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
 
 /**
  * The Class ApplicationConfiguration.
@@ -11,4 +19,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfiguration {
 
+	/**
+	 * Configurer.
+	 *
+	 * @param applicationName the application name
+	 * @return the meter registry customizer
+	 */
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") final String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
+
+	/**
+	 * Process memory metrics.
+	 *
+	 * @return the meter binder
+	 */
+	@Bean
+	public MeterBinder processMemoryMetrics() {
+		return new ProcessMemoryMetrics();
+	}
+
+	/**
+	 * Process thread metrics.
+	 *
+	 * @return the meter binder
+	 */
+	@Bean
+	public MeterBinder processThreadMetrics() {
+		return new ProcessThreadMetrics();
+	}
 }
